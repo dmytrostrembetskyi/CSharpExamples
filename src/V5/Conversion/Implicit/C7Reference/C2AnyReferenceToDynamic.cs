c2c5e7554bd87c088541aeaa880f55292eff153f
DmytroStrembetskyi <dmytrostrembetskyi@gmail.com>
2021-08-13 18:03:01 +0300

    add implicit reference conversion

---
+++ src/V5/Conversion/Implicit/C7Reference/C2AnyReferenceToDynamic.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+// ReSharper disable SuggestVarOrType_SimpleTypes
+
+namespace V5.Conversion.Implicit.C7Reference
+{
+    public class C2AnyReferenceToDynamic
+    {
+        public C2AnyReferenceToDynamic()
+        {
+            Decoder a = Encoding.UTF8.GetDecoder();
+            object a2 = a;
+        }
+    }
+}
---
+++ src/V5/Conversion/Implicit/C7Reference/C3Derived.cs
@@ -0,0 +1,15 @@
+﻿using V5.Utils;
+
+// ReSharper disable SuggestVarOrType_SimpleTypes
+
+namespace V5.Conversion.Implicit.C7Reference
+{
+    public class C3Derived
+    {
+        public C3Derived()
+        {
+            SimpleChild a = new SimpleChild();
+            SimpleParent a2 = a;
+        }
+    }
+}
