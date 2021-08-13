553b9a99a9445edab672b519d31bc124b09248a8
DmytroStrembetskyi <dmytrostrembetskyi@gmail.com>
2021-08-13 15:58:07 +0300

    implicit conversion examples

---
+++ src/V5/Conversion/Implicit/NullLiteral/ToAnyNullableType.cs
@@ -0,0 +1,11 @@
+﻿namespace V5.Conversion.Implicit.NullLiteral
+{
+    public class ToAnyNullableType
+    {
+        public ToAnyNullableType()
+        {
+            string a = null;
+            int? b = null;
+        }
+    }
+}
---
+++ src/V5/Conversion/Implicit/Nullable/C3_DifferentType_WithNull.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable ExpressionIsAlwaysNull
+
+namespace V5.Conversion.Implicit.Nullable
+{
+    public class C3_DifferentType_WithNull
+    {
+        public C3_DifferentType_WithNull()
+        {
+            int? a = null;
+            long? a2 = a;
+
+            int? b = null;
+            // long b2 = a;
+        }
+    }
+}
