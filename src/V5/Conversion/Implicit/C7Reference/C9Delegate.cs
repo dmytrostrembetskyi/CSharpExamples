129f49e46c135a7742807d804563c1e8a9d9c9b3
DmytroStrembetskyi <dmytrostrembetskyi@gmail.com>
2021-08-14 09:07:43 +0300

    delegate

--- src/V5/Conversion/Implicit/C7Reference/C9Delegate.cs
+++ src/V5/Conversion/Implicit/C7Reference/C9Delegate.cs
@@ -1,10 +1,26 @@
-﻿namespace V5.Conversion.Implicit.C7Reference
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace V5.Conversion.Implicit.C7Reference
 {
     public class C9Delegate
     {
         public C9Delegate()
         {
-            
+            DelegateExample a = Method;
+            Delegate a2 = a;
+
+            DelegateExample b = Method;
+            ICloneable b2 = b;
+
+            DelegateExample c = Method;
+            ISerializable c2 = c;
         }
+
+        void Method()
+        {
+        }
+
+        delegate void DelegateExample();
     }
 }
