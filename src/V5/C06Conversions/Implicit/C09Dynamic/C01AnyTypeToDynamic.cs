95ac3cee902b7219b9fd2572e08172fa65dfe987
DmytroStrembetskyi <dmytrostrembetskyi@gmail.com>
2021-09-08 15:35:26 +0300

    update explicit variable type

--- src/V5/C06Conversions/Implicit/C09Dynamic/C01AnyTypeToDynamic.cs
+++ src/V5/C06Conversions/Implicit/C09Dynamic/C01AnyTypeToDynamic.cs
@@ -1,29 +1,26 @@
-﻿namespace V5.C06Conversions.Implicit.C09Dynamic
+﻿// ReSharper disable SuggestVarOrType_BuiltInTypes
+// ReSharper disable SuggestVarOrType_SimpleTypes
+
+namespace V5.C06Conversions.Implicit.C09Dynamic
 {
     public class C01AnyTypeToDynamic
     {
         public C01AnyTypeToDynamic()
         {
-            var a1 = 1;
+            int a1 = 1;
             dynamic a2 = a1;
-            // int a3 = a2;
 
-            var b1 = new object();
+            object b1 = new object();
             dynamic b2 = b1;
-            object b3 = b2;
 
-            var c1 = Enum.First;
+            Enum c1 = Enum.First;
             dynamic c2 = c1;
-            // Enum c3 = c2;
 
             dynamic d1 = "";
-            var d2 = d1;
-            var d3 = d2;
+            dynamic d2 = d1;
 
-            var e1 = "";
+            string e1 = "";
             dynamic e2 = e1;
-            // string e3 = e2;
-            object e4 = e2;
         }
 
         enum Enum
